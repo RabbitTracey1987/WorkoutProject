9f7b8900b43847d36f8e82c03841d7a568d74306
Ji Wang <traceyw@vt.edu>
2018-08-28 19:40:06 +0800

    Create exercise model and association

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -3,6 +3,7 @@ RSpec.feature "Creating Exercise" do
 	before do
 		@john = User.create!(email: "john@example.com", password: "password")
 		login_as(@john)
+	end
 	scenario "with valid inputs" do 
   	visit "/"
   	click_link "My Lounge"
