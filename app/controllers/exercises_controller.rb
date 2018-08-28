9c0209618a0ee71ca28a223b39c9f9c410051c2e
Ji Wang <traceyw@vt.edu>
2018-08-28 20:27:17 +0800

    add exercise model and _form partial

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,4 +4,6 @@ class ExercisesController < ApplicationController
 	def new
 		@exercise=current_user.exercises.new
 	end
+	def create
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :exercises
+  def full_name
+  	[first_name,last_name].join
+  end
 end
