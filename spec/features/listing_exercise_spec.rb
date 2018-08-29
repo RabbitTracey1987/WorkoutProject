60d7bae93fe276b6e3d665ea615a9bf4211811f5
Ji Wang <traceyw@vt.edu>
2018-08-29 13:13:15 +0800

    display message if no exercises created

--- spec/features/listing_exercise_spec.rb
+++ spec/features/listing_exercise_spec.rb
@@ -6,15 +6,14 @@ RSpec.feature "Listing Exercise" do
     @e1=@john.exercises.create(duration_in_min:20,
                                workout:"My body exercise1",
                                workout_date:Date.today)
-      )
+      
     @e2=@john.exercises.create(duration_in_min:50,
                                workout:"Weight lifting",
                                workout_date:2.days.ago)
-      )
-    visit "/"
-    click_link "My Lounge"
 	end
 	scenario "with valid inputs" do 
+    visit "/"
+    click_link "My Lounge"
   	expect(page).to have_content(@e1.duration_in_min)
     expect(page).to have_content(@e1.workout)
     expect(page).to have_content(@e1.workout_date)
@@ -25,4 +24,11 @@ RSpec.feature "Listing Exercise" do
     # expect(page).to have_content(@e3.workout)
     # expect(page).to have_content(@e3.workout_date) 	
 	end
+  scenario "show no exercise is none created" do 
+    @john.exercises.delete_all
+    visit "/"
+    click_link "My Lounge"
+    expect(page).to have_content("No Workouts Yet")
+
+  end
 end
