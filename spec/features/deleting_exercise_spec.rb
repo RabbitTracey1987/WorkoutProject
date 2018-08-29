0589efe042d8d2d9c78fbd3e3968294af838d227
Ji Wang <traceyw@vt.edu>
2018-08-29 15:04:18 +0800

    write deleting exercise feature

---
+++ spec/features/deleting_exercise_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+RSpec.feature "Editing Exercise" do 
+	before do
+		@john = User.create!(email: "john@example.com", password: "password")
+    @john_exercise = @john.exercises.create!(duration_in_min:30,
+                                              workout:"first activity",
+                                              workout_date: Date.today)
+		login_as(@john)
+	end
+	scenario "with valid data succeed" do 
+    visit "/"
+    click_link "My Lounge"
+    path= "/users/#{@john.id}/exercises/#{@john_exercise.id}"
+    link= "//a[href=\'#{path}\'] and .//text()='Destroy']"
+    find(:xpath,link).click
+    fill_in "Duration", with: 40
+    click_button "Update Exercise"
+  	expect(page).to have_content("Exercise has been deleted")
+	end
+ 
+end
