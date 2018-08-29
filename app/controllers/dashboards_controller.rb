cba7de3fa66bc424717aac3a280c62d0cbb19e6f
Ji Wang <traceyw@vt.edu>
2018-08-29 17:27:09 +0800

    implement searching for users

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,4 +2,7 @@ class DashboardsController < ApplicationController
 	def index
 		@atheletes=User.paginate(:page => params[:page])
 	end
+	def search 
+		@atheletes=User.search_by_name(params[:search_name]).paginate(page:params[:page])
+	end
 end
