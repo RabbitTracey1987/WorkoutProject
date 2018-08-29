e92f98635d70787701132b3cd319ff951d184984
Ji Wang <traceyw@vt.edu>
2018-08-29 16:02:01 +0800

    Add first and last names to signup form

--- spec/features/listing_exercise_spec.rb
+++ spec/features/listing_exercise_spec.rb
@@ -10,6 +10,9 @@ RSpec.feature "Listing Exercise" do
     @e2=@john.exercises.create(duration_in_min:50,
                                workout:"Weight lifting",
                                workout_date:2.days.ago)
+    @e3=@john.exercises.create(duration_in_min:80,
+                               workout:"Abs Exercise",
+                               workout_date:8.days.ago)
 	end
 	scenario "with valid inputs" do 
     visit "/"
@@ -20,9 +23,9 @@ RSpec.feature "Listing Exercise" do
     expect(page).to have_content(@e2.duration_in_min)
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
-    # expect(page).to have_content(@e3.duration_in_min)
-    # expect(page).to have_content(@e3.workout)
-    # expect(page).to have_content(@e3.workout_date) 	
+    expect(page).not_to have_content(@e3.duration_in_min)
+    expect(page).not_to have_content(@e3.workout)
+    expect(page).not_to have_content(@e3.workout_date) 	
 	end
   scenario "show no exercise is none created" do 
     @john.exercises.delete_all
