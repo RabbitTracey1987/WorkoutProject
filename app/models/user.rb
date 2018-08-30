e5c2618eadfd516fe67ed01e46f9132d29b9ab0d
Ji Wang <traceyw@vt.edu>
2018-08-30 19:14:27 +0800

    modify user workout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :exercises
   has_many :friendships
-  has_many :friends,through: :friendships, class_name: "User"
+  has_many :friends, through: :friendships, class_name: "User"
   validates :first_name, presence: true
   validates :last_name, presence: true
   self.per_page=10
