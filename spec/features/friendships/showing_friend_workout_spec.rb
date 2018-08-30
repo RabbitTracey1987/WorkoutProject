81cef96e08c2e7b3c795e305f872e249bf074f61
Ji Wang <traceyw@vt.edu>
2018-08-30 15:03:25 +0800

    Implement showing friends

---
+++ spec/features/friendships/showing_friend_workout_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+RSpec.feature "Showing Friend Workout" do 
+	before do
+		@john = User.create!(first_name:"John",last_name:"Smith",email: "john@example.com", password: "password")
+		login_as(@john)
+    @sarah = User.create!(first_name:"Sarah",last_name:"Wang",email: "sarah@example.com", password: "password")
+    login_as(@john)
+    @e1=@john.exercises.create(duration_in_min:20,
+                               workout:"My body exercise1",
+                               workout_date:Date.today)
+      
+    @e2=@sarah.exercises.create(duration_in_min:50,
+                               workout:"Weight lifting",
+                               workout_date:2.days.ago)
+    @following = Friendship.create(user:@john,friend: @sarah)
+   
+	end
+  scenario "show a list of user's friends" do 
+    visit "/"
+    click_link "My Lounge"
+    click_link @sarah.full_name
+    expect(page).to have_content(@sarah.full_name+"'s Exercises")
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_css("div#chart")
+  end
+
+end
