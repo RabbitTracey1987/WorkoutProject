43b962a1e62d1a49fb5fdebd3e9a5463d7dc8ffb
Ji Wang <traceyw@vt.edu>
2018-08-31 19:53:56 +0800

    fix some syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-	#protect_from_forgery with: :exception
-
+	protect_from_forgery with: :exception
 	private 
 	def current_room
 		@room ||= Room.find(session[:current_room]) if session[:current_room]
